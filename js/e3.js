2af4b540d0629e3c51c3d3401d1c93e0eb9c2006
Frank FitzGerald <me@frankfitzgerald.net>
2014-05-02 09:37:26 -0400

    Update e3.js

--- js/e3.js
+++ js/e3.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 		e.preventDefault();
 		$.ajax({
 			data: 'zip=' + $("#search input[type='text']").val() + '&output=json',
-			dataType: 'json',
+			// dataType: 'json',
 			type: 'get',
 			url: 'getReps.php',
 			success: function(responseData) {
