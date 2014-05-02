36afb1b039956cf4f82a7b192e93a93d4a0558c1
Frank FitzGerald <me@frankfitzgerald.net>
2014-05-02 09:45:37 -0400

    Update e3.js

--- js/e3.js
+++ js/e3.js
@@ -3,14 +3,15 @@
    ========================================================================== */
 
 $(document).ready(function() {
-    $('form').bind('submit',function(e) {
+	$('form').bind('submit',function(e) {
 		e.preventDefault();
 		$.ajax({
 			data: 'zip=' + $("#search input[type='text']").val() + '&output=json',
 			dataType: 'json',
 			type: 'get',
-			url: 'getReps.php',
+			url: 'http://whoismyrepresentative.com/getall_mems.php',
 			success: function(responseData) {
+				find_reps(responseData);
 				$('#main').html(responseData);
 			},
 			error: function(responseData) {
@@ -18,4 +19,26 @@ $(document).ready(function() {
 			}
 		});
 	});
+	function find_reps (jsonObject) {
+		// var zip_encoded = urlencode(zip);
+		// echo $zip_encoded;
+	
+		// var url = "http://whoismyrepresentative.com/getall_mems.php";
+		// url += "?zip=" + "" + zip_encoded;
+		// url += "&output=json";
+	  	// print "<p>$url</p>";
+	
+		// var feed = getResource(url);
+		var feed = jsonObject;
+		var jsonObject = json_decode(feed);
+		// print_r($jsonObject);
+	
+		for (var resultKey in jsonObject.results) {		
+	                 var result = jsonObject.results[resultKey];
+			var name = result.name;
+			var getlastname = preg_split('/ /', name);
+			find_twitter_username(getlastname[0], getlastname[1]);
+		}
+	
+	} // find_reps
 });
