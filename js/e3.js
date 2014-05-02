855d17baa01d74b9e790dd6af164b06f50492c76
Frank FitzGerald <me@frankfitzgerald.net>
2014-05-02 11:09:05 -0400

    Update e3.js

--- js/e3.js
+++ js/e3.js
@@ -16,56 +16,44 @@ $(document).ready(function() {
 			// },
 			success: function(responseData){
 				console.log('Success Bitches!');
+				var feed = responseData;
+				var jsonObject = json_decode(feed);
+				// print_r($jsonObject);
+				 "<ul>"; 
+				for (var responseKey in jsonObject.response) {		
+			                 var response = jsonObject.response[responseKey];
+					firstname = response[0].legislator.firstname;
+					lastname = response[0].legislator.lastname;
+					var twitterid = response[0].legislator.twitter_id;
+					var chamber = response[0].legislator.chamber;
+					var party = response[0].legislator.party;
+					var gender = response[0].legislator.gender;
+					if (gender === 'M'){
+						gender = 'Sir';
+					}else{
+						gender = 'Maam';
+					}
+					if (party === 'R'){
+						party = 'Republican';
+					}else{
+						party = 'Democrat';
+					}
+					 '<span>Name:</span><li>' + "" + firstname + "" + ' ' + "" + lastname + "" + '</li>';
+					 '<span>Chamber:</span><li>' + "" + ucfirst(chamber) + "" + '</li>';
+					 '<span>Party:</span><li>' + "" + party + "" + '</li>';
+					if (twitterid === ''){
+						 "<li class='clearfix'><span class='fail_icon'></span> No Twitter, you fail " + gender + ".</li>";
+					}else{
+						 "<li class='clearfix'><span class='twit_icon'></span> <a href='http://twitter.com/" + twitterid + "' target='new'>@" + twitterid + "</a></li>";
+					}
+				}
+			// # print <ul> closing
+				 "</ul>";
+				$('#main').html();
 			},
 			error: function(responseData) {
 				console.log('the getReps.php ajax call failed');
 			}
 		});
 	});
-	function find_reps (jsonObject) {
-		// var zip_encoded = urlencode(zip);
-		// echo $zip_encoded;
-	
-		// var url = "http://whoismyrepresentative.com/getall_mems.php";
-		// url += "?zip=" + "" + zip_encoded;
-		// url += "&output=json";
-	  	// print "<p>$url</p>";
-	
-		// var feed = getResource(url);
-		var feed = jsonObject;
-		var jsonObject = json_decode(feed);
-		// print_r($jsonObject);
-		 "<ul>"; 
-		for (var responseKey in jsonObject.response) {		
-	                 var response = jsonObject.response[responseKey];
-			firstname = response[0].legislator.firstname;
-			lastname = response[0].legislator.lastname;
-			var twitterid = response[0].legislator.twitter_id;
-			var chamber = response[0].legislator.chamber;
-			var party = response[0].legislator.party;
-			var gender = response[0].legislator.gender;
-			if (gender === 'M'){
-				gender = 'Sir';
-			}else{
-				gender = 'Maam';
-			}
-			if (party === 'R'){
-				party = 'Republican';
-			}else{
-				party = 'Democrat';
-			}
-			 '<span>Name:</span><li>' + "" + firstname + "" + ' ' + "" + lastname + "" + '</li>';
-			 '<span>Chamber:</span><li>' + "" + ucfirst(chamber) + "" + '</li>';
-			 '<span>Party:</span><li>' + "" + party + "" + '</li>';
-			if (twitterid === ''){
-				 "<li class='clearfix'><span class='fail_icon'></span> No Twitter, you fail " + gender + ".</li>";
-			}else{
-				 "<li class='clearfix'><span class='twit_icon'></span> <a href='http://twitter.com/" + twitterid + "' target='new'>@" + twitterid + "</a></li>";
-			}
-		}
-	// # print <ul> closing
-		 "</ul>";
-		$('#main').html();
-	
-	} // find_reps
 });
